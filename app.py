5505e2dd85d299ba4ed3ef418b98ec73af523103
papasim824 <papasim824@gmail.com>
2023-08-02 00:17:26 +0900

    mod

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import json
 import urllib.request
-from PIL import Image
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-from janome.tokenizer import Tokenizer
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
+from janome.tokenizer import Tokenizer
+from PIL import Image
+from wordcloud import WordCloud
 
 
 def hide_style():
@@ -46,31 +46,36 @@ def hide_style():
                     """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+
 @st.cache_data(show_spinner=False)
 def get_word_str(text):
-    word_list=[]
+    word_list = []
     t = Tokenizer()
     for token in t.tokenize(text):
-        split_token = token.part_of_speech.split(',')
+        split_token = token.part_of_speech.split(",")
         ## 一般名詞を抽出
-        if split_token[0] == '名詞' and split_token[1] == '一般':
+        if split_token[0] == "名詞" and split_token[1] == "一般":
             word_list.append(token.surface)
-    split_text=" ".join(word_list)
+    split_text = " ".join(word_list)
     return split_text
 
-@st.cache_data(experimental_allow_widgets=True,show_spinner=False)
+
+@st.cache_data(experimental_allow_widgets=True, show_spinner=False)
 def show_wordcloud(text):
     with st.spinner("Wordcloud作成中・・・"):
         # Create some sample text
         split_text = get_word_str(text)
 
         # Create and generate a word cloud image:
-        wordcloud = WordCloud(font_path="ipaexg.ttf",background_color='white').generate(split_text)
+        wordcloud = WordCloud(
+            font_path="ipaexg.ttf", background_color="white"
+        ).generate(split_text)
 
         # Display the generated image:
-        plt.imshow(wordcloud, interpolation='bilinear')
-        plt.axis("off") 
+        plt.imshow(wordcloud, interpolation="bilinear")
+        plt.axis("off")
         st.pyplot(plt)
+
 
 # クエリパラメータを組み立てる(1ページ100件固定)
 def prepare_parameter(query):
@@ -89,8 +94,16 @@ def prepare_headers(token):
 
     return req_headers
 
-def create_metric(wch_colour_box=(87,204,0),wch_colour_font= (255,255,255),fontsize = 36,valign = "left",iconname="fas fa-asterisk",sline = "",value=""):
-    
+
+def create_metric(
+    wch_colour_box=(87, 204, 0),
+    wch_colour_font=(255, 255, 255),
+    fontsize=36,
+    valign="left",
+    iconname="fas fa-asterisk",
+    sline="",
+    value="",
+):
     lnk = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" crossorigin="anonymous">'
 
     htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, 
@@ -112,30 +125,35 @@ def create_metric(wch_colour_box=(87,204,0),wch_colour_font= (255,255,255),fonts
 
     st.markdown(lnk + htmlstr, unsafe_allow_html=True)
 
+
 def show_articles(value):
-    create_metric(sline = "Articles",value=value,iconname="fa fa-newspaper")
+    create_metric(sline="Articles", value=value, iconname="fa fa-newspaper")
+
 
 def show_view_total(value):
-    create_metric(sline = "Views",value=value,iconname="fa fa-eye")
+    create_metric(sline="Views", value=value, iconname="fa fa-eye")
+
 
 def show_likes_total(value):
-    create_metric(sline = "Likes",value=value,iconname="fa fa-thumbs-up")
+    create_metric(sline="Likes", value=value, iconname="fa fa-thumbs-up")
+
 
 def show_stocks_total(value):
-    create_metric(sline = "Stocks",value=value,iconname="fa fa-bookmark")
+    create_metric(sline="Stocks", value=value, iconname="fa fa-bookmark")
+
 
 def show_comments_total(value):
-    create_metric(sline = "Comments",value=value,iconname="fa fa-comment")
+    create_metric(sline="Comments", value=value, iconname="fa fa-comment")
 
 
-def pagenation_by_total_likes(item_ids,token=None, query=None):
+def pagenation_by_total_likes(item_ids, token=None, query=None):
     df_likes = pd.DataFrame()
     # クエリパラメータの準備
     params = prepare_parameter(query)
     # アクセストークンが指定された場合に付与
     req_headers = prepare_headers(token)
 
-    for num,item_id in enumerate(item_ids):
+    for num, item_id in enumerate(item_ids):
         with st.spinner(f"記事ごとの情報を取得中：{num+1}/{len(item_ids)}"):
             for page_num_likes in range(1, 101):
                 params["page"] = str(page_num_likes)
@@ -145,7 +163,7 @@ def pagenation_by_total_likes(item_ids,token=None, query=None):
                 )
 
                 req_likes = urllib.request.Request(url_likes, headers=req_headers)
-                
+
                 with urllib.request.urlopen(req_likes) as res_likes:
                     # DataFrameに記事情報を格納
                     df = pd.json_normalize(json.load(res_likes))
@@ -157,16 +175,17 @@ def pagenation_by_total_likes(item_ids,token=None, query=None):
                     if page_num_likes >= (total_count_likes + 99) // 100:
                         break
     if len(df_likes):
-        df_likes["created_at"]=df_likes["created_at"].str[0:13]+":00:00"
-        df_likes["created_at"]=pd.to_datetime(df_likes["created_at"])
-        df_likes["created_at"]=df_likes["created_at"].dt.strftime('%Y/%m/%d')
+        df_likes["created_at"] = df_likes["created_at"].str[0:13] + ":00:00"
+        df_likes["created_at"] = pd.to_datetime(df_likes["created_at"])
+        df_likes["created_at"] = df_likes["created_at"].dt.strftime("%Y/%m/%d")
 
     st.session_state.df_likes = df_likes
+
 
 @st.cache_data(show_spinner=False)
 def pagenation_by_total_count(token=None, query=None):
     df_ret = pd.DataFrame()
-    
+
     # クエリパラメータの準備
     params = prepare_parameter(query)
     # アクセストークンが指定された場合に付与
@@ -189,11 +208,12 @@ def pagenation_by_total_count(token=None, query=None):
                 if page_num >= (total_count + 99) // 100:
                     break
     item_ids = df_ret["id"].to_list()
-            
+
     return df_ret, item_ids
 
+
 @st.cache_data(show_spinner=False)
-def get_user_info(token,user_name):
+def get_user_info(token, user_name):
     url = f"https://qiita.com/api/v2/users/{urllib.parse.quote(user_name)}"
     req_headers = prepare_headers(token)
 
@@ -206,17 +226,15 @@ def get_user_info(token,user_name):
         st.stop()
     return body
 
+
 def main():
     st.set_page_config(
-        page_title="Qiiboard",
-        page_icon=Image.open('favicon.png'),
-        layout="wide"
+        page_title="Qiiboard", page_icon=Image.open("favicon.png"), layout="wide"
     )
     hide_style()
 
     if "df_likes" not in st.session_state:
         st.session_state.df_likes = pd.DataFrame()
-
 
     sort_options = {
         "📝作成日": "created_at",
@@ -225,15 +243,17 @@ def main():
         "👀閲覧数": "page_views_count",
         "💬コメント数": "comments_count",
     }
-    with st.sidebar:   
+    with st.sidebar:
         with st.form("info"):
-            accsess_token = st.text_input("Access Token",placeholder="YOUR ACCESS TOKEN")
-            user_name = st.text_input("User Name",placeholder="Qiita User Name")
+            accsess_token = st.text_input(
+                "Access Token", placeholder="YOUR ACCESS TOKEN"
+            )
+            user_name = st.text_input("User Name", placeholder="Qiita User Name")
             st.form_submit_button("データ取得")
-    
+
     st.image("logo.png")
-    if all([user_name,accsess_token]):
-        if user_info:=get_user_info(accsess_token,user_name):
+    if all([user_name, accsess_token]):
+        if user_info := get_user_info(accsess_token, user_name):
             html = f"""
             <style>
             .avatar {{
@@ -249,28 +269,30 @@ def main():
             {user_name}
             </h2>
             """
-            
+
             st.write("---")
-            st.write(html,unsafe_allow_html=True)
+            st.write(html, unsafe_allow_html=True)
             # Total-Countヘッダを利用して、「QiitaAPI」タグの記事を取得
             df_total_count, item_ids = pagenation_by_total_count(
                 token=accsess_token, query=f"user:{user_name}"
             )
-            df_total_count["created_at"] =pd.to_datetime(df_total_count["created_at"])
-            df_total_count["created_at"] =df_total_count["created_at"].dt.strftime('%Y/%m/%d %H:%M:%S')
+            df_total_count["created_at"] = pd.to_datetime(df_total_count["created_at"])
+            df_total_count["created_at"] = df_total_count["created_at"].dt.strftime(
+                "%Y/%m/%d %H:%M:%S"
+            )
             body_list = df_total_count["body"].to_list()
             title_list = df_total_count["title"].to_list()
 
-            word_list = body_list+title_list
-            wordcloud_text="".join(word_list)
+            word_list = body_list + title_list
+            wordcloud_text = "".join(word_list)
 
-            articles = f'{len(df_total_count):,}'
-            view_total=f'{df_total_count["page_views_count"].sum():,}'
-            likes_total=f'{df_total_count["likes_count"].sum():,}'
-            stocks_total=f'{df_total_count["stocks_count"].sum():,}'
-            comments_total=f'{df_total_count["comments_count"].sum():,}'
+            articles = f"{len(df_total_count):,}"
+            view_total = f'{df_total_count["page_views_count"].sum():,}'
+            likes_total = f'{df_total_count["likes_count"].sum():,}'
+            stocks_total = f'{df_total_count["stocks_count"].sum():,}'
+            comments_total = f'{df_total_count["comments_count"].sum():,}'
 
-            cols=st.columns(5)
+            cols = st.columns(5)
             with cols[0]:
                 show_articles(articles)
             with cols[1]:
@@ -292,18 +314,25 @@ def main():
                 for id, sdf in df_total_count.sort_values(
                     sort_options[sort_value_jp], ascending=False
                 ).groupby("id", sort=False):
-                    likes=pd.DataFrame()
+                    likes = pd.DataFrame()
 
                     if len(df_likes):
-                        likes = df_likes[df_likes["id"]==id][["created_at","id"]].groupby("created_at").count()
+                        likes = (
+                            df_likes[df_likes["id"] == id][["created_at", "id"]]
+                            .groupby("created_at")
+                            .count()
+                        )
                         likes["likes"] = likes["id"].cumsum()
-                        likes=likes.drop("id",axis=1)
+                        likes = likes.drop("id", axis=1)
 
-                    title=sdf["title"].values[0]
+                    title = sdf["title"].values[0]
                     title_caption = sdf[sort_options[sort_value_jp]].values[0]
                     with st.expander(f"【{sort_value_jp} / {title_caption}】\t{title} "):
-                        st.write(f'<a href="{sdf["url"].values[0]}">{title}</a>',unsafe_allow_html=True)
-                        cols=st.columns(4)
+                        st.write(
+                            f'<a href="{sdf["url"].values[0]}">{title}</a>',
+                            unsafe_allow_html=True,
+                        )
+                        cols = st.columns(4)
                         with cols[0]:
                             show_view_total(sdf["page_views_count"].values[0])
                         with cols[1]:
@@ -314,11 +343,18 @@ def main():
                             show_comments_total(sdf["comments_count"].values[0])
                         st.line_chart(likes)
             else:
-                st.button("記事ごとの情報を取得する",help="取得に時間がかかる場合があります。",on_click=pagenation_by_total_likes,args=(item_ids,accsess_token, f"user:{user_name}",))
-
+                if st.button("記事ごとの情報を取得する", help="取得に時間がかかる場合があります。"):
+                    pagenation_by_total_likes(
+                        item_ids, accsess_token, f"user:{user_name}"
+                    )
+                    st.experimental_rerun()
 
     else:
-        st.info("アクセストークンとユーザー名を入力してください。\n\n[アクセストークンの取得方法](https://github.com/ppspps824/Qiiboard)",icon="👈")
-            
+        st.info(
+            "アクセストークンとユーザー名を入力してください。\n\n[アクセストークンの取得方法](https://github.com/ppspps824/Qiiboard)",
+            icon="👈",
+        )
+
+
 if __name__ == "__main__":
     main()
