92314a36f3565ed2ca3a6d74bbffa7b86996302a
papasim824 <example@example.com>
2023-08-05 23:15:01 +0900

    new

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Qiiboardとは
+Qiitaの各種情報をまとめたダッシュボードを作成するWebアプリです。
+記事の総数、総閲覧数、総いいね数、総ストック数、記事タイトル・本文をもとにしたワードクラウド、いいねの推移等が取得できます。
+
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://qiiboard.streamlit.app/)
+
+### 開始方法
+- Qiitaの設定/アプリケーション/個人用アクセストークンより新しくトークンを発行します。(スコープは「read_qiita」がついていれば問題ありません。) 
+- 取得したアクセストークンを「Access Token」に入力します。
+- User Nameに取得したいユーザーのユーザー名を入力します。
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/papasim824C)
---
+++ app.py
@@ -0,0 +1,297 @@
+import json
+import urllib.request
+from PIL import Image
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from janome.tokenizer import Tokenizer
+
+import pandas as pd
+import streamlit as st
+
+
+def hide_style():
+    hide_streamlit_style = """
+                    <style>
+                    div[data-testid="stToolbar"] {
+                    visibility: hidden;
+                    height: 0%;
+                    position: fixed;
+                    }
+                    div[data-testid="stDecoration"] {
+                    visibility: hidden;
+                    height: 0%;
+                    position: fixed;
+                    }
+                    div[data-testid="stStatusWidget"] {
+                    visibility: hidden;
+                    height: 0%;
+                    position: fixed;
+                    }
+                    #MainMenu {
+                    visibility: hidden;
+                    height: 0%;
+                    }
+                    header {
+                    visibility: hidden;
+                    height: 0%;
+                    }
+                    footer {
+                    visibility: hidden;
+                    height: 0%;
+                    }
+                    .block-container {
+                    padding-top: 2rem;
+                    }
+                    </style>
+                    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+@st.cache_data()
+def get_word_str(text):
+    word_list=[]
+    t = Tokenizer()
+    for token in t.tokenize(text):
+        split_token = token.part_of_speech.split(',')
+        ## 一般名詞を抽出
+        if split_token[0] == '名詞' and split_token[1] == '一般':
+            word_list.append(token.surface)
+    split_text=" ".join(word_list)
+    return split_text
+
+@st.cache_data(experimental_allow_widgets=True)
+def show_wordcloud(text):
+    # Create some sample text
+    split_text = get_word_str(text)
+
+    # Create and generate a word cloud image:
+    wordcloud = WordCloud(font_path="ipaexg.ttf",background_color='white').generate(split_text)
+
+    # Display the generated image:
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off") 
+    st.pyplot(plt)
+
+# クエリパラメータを組み立てる(1ページ100件固定)
+def prepare_parameter(query):
+    params = {"per_page": "100"}
+    if query is not None:
+        params["query"] = query
+
+    return params
+
+
+# 認証トークンが指定された場合にヘッダに付与する
+def prepare_headers(token):
+    req_headers = {}
+    if token is not None:
+        req_headers = {"Authorization": "Bearer " + token}
+
+    return req_headers
+
+def create_metric(wch_colour_box=(87,204,0),wch_colour_font= (255,255,255),fontsize = 36,valign = "left",iconname="fas fa-asterisk",sline = "",value=""):
+    
+    lnk = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" crossorigin="anonymous">'
+
+    htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, 
+                                                {wch_colour_box[1]}, 
+                                                {wch_colour_box[2]}, 1); 
+                            color: rgb({wch_colour_font[0]}, 
+                                    {wch_colour_font[1]}, 
+                                    {wch_colour_font[2]}, 1); 
+                            font-size: {fontsize}px; 
+                            border-radius: 7px; 
+                            padding-left: 12px; 
+                            padding-top: 18px; 
+                            padding-bottom: 18px; 
+                            line-height:25px;'>
+                            <i class='{iconname} fa-xs'></i> {value}
+                            <BR><span style='font-size: 14px; 
+                            margin-top: 0;'>{sline}</style></span>
+                            </style></p>"""
+
+    st.markdown(lnk + htmlstr, unsafe_allow_html=True)
+
+def show_articles(value):
+    create_metric(sline = "Articles",value=value,iconname="fa fa-newspaper")
+
+def show_view_total(value):
+    create_metric(sline = "Views",value=value,iconname="fa fa-eye")
+
+def show_likes_total(value):
+    create_metric(sline = "Likes",value=value,iconname="fa fa-thumbs-up")
+
+def show_stocks_total(value):
+    create_metric(sline = "Stocks",value=value,iconname="fa fa-bookmark")
+
+@st.cache_data()
+def pagenation_by_total_count(token=None, query=None):
+    df_ret = pd.DataFrame()
+    df_likes = pd.DataFrame()
+
+    # クエリパラメータの準備
+    params = prepare_parameter(query)
+    # アクセストークンが指定された場合に付与
+    req_headers = prepare_headers(token)
+
+    for page_num in range(1, 101):
+        params["page"] = str(page_num)
+        url = "https://qiita.com/api/v2/items?" + urllib.parse.urlencode(params)
+
+        req = urllib.request.Request(url, headers=req_headers)
+        with urllib.request.urlopen(req) as res:
+            body = json.load(res)
+            # DataFrameに記事情報を格納
+            df = pd.json_normalize(body)
+            df_ret = pd.concat([df_ret, df])
+            print("Page: " + str(page_num))
+            # Total-Countヘッダの値から最後のページまで取得したかを判断
+            total_count = int(res.headers["Total-Count"])
+            if page_num >= (total_count + 99) // 100:
+                break
+
+    item_ids = df_ret["id"].to_list()
+    
+    for item_id in item_ids:
+        for page_num_likes in range(1, 101):
+            params["page"] = str(page_num_likes)
+            url_likes = (
+                f"https://qiita.com/api/v2/items/{item_id}/likes?"
+                + urllib.parse.urlencode(params)
+            )
+
+            req_likes = urllib.request.Request(url_likes, headers=req_headers)
+            
+
+            with urllib.request.urlopen(req_likes) as res_likes:
+                # DataFrameに記事情報を格納
+                df = pd.json_normalize(json.load(res_likes))
+                df["id"] = item_id
+                df_likes = pd.concat([df_likes, df])
+                print("Page: " + str(page_num_likes))
+                # Total-Countヘッダの値から最後のページまで取得したかを判断
+                total_count_likes = int(res_likes.headers["Total-Count"])
+                if page_num_likes >= (total_count_likes + 99) // 100:
+                    break
+            
+    return df_ret, df_likes
+
+@st.cache_data()
+def get_user_info(token,user_name):
+    url = f"https://qiita.com/api/v2/users/{urllib.parse.quote(user_name)}"
+    req_headers = prepare_headers(token)
+
+    req = urllib.request.Request(url, headers=req_headers)
+    try:
+        with urllib.request.urlopen(req) as res:
+            body = json.load(res)
+    except urllib.error.HTTPError as err:
+        st.error("ユーザ情報を取得できませんでした。")
+        st.stop()
+
+    return body
+
+def main():
+    st.set_page_config(
+        page_title="Qiiboard",
+        page_icon=Image.open('favicon.png'),
+        layout="wide"
+    )
+    hide_style()
+    # accsess_token = st.secrets["Qiita_API_KEY"]
+    sort_options = {
+        "📝作成日": "created_at",
+        "👍いいね数": "likes_count",
+        "📚ストック数": "stocks_count",
+        "👀閲覧数": "page_views_count",
+        "💬コメント数": "comments_count",
+    }
+    with st.sidebar:   
+        accsess_token = st.text_input("Access Token",placeholder="YOUR ACCESS TOKEN")
+        user_name = st.text_input("User Name",placeholder="Qiita User Name")
+        if user_name:
+            sort_value_jp = st.selectbox("Sort", options=sort_options.keys())
+    
+    st.image("logo.png")
+    if user_name:
+        user_info = get_user_info(accsess_token,user_name)
+        if user_info:
+            html = f"""
+
+    <style>
+    .avatar {{
+    vertical-align: middle;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    }}  
+    </style>
+
+    <h2>
+    <a href="https://qiita.com/{user_name}"><img src="{user_info['profile_image_url']}" alt="Avatar" class="avatar"></a>
+    {user_name}
+    </h2>
+    """
+            
+            st.write("---")
+            st.write(html,unsafe_allow_html=True)
+            # Total-Countヘッダを利用して、「QiitaAPI」タグの記事を取得
+            df_total_count, df_likes = pagenation_by_total_count(
+                token=accsess_token, query=f"user:{user_name}"
+            )
+            df_total_count["created_at"] =pd.to_datetime(df_total_count["created_at"])
+            df_total_count["created_at"] =df_total_count["created_at"].dt.strftime('%Y/%m/%d %H:%M:%S')
+            body_list = df_total_count["body"].to_list()
+            title_list = df_total_count["title"].to_list()
+
+            word_list = body_list+title_list
+            wordcloud_text="".join(word_list)
+
+            articles = f'{len(df_total_count):,}'
+            view_total=f'{df_total_count["page_views_count"].sum():,}'
+            likes_total=f'{df_total_count["likes_count"].sum():,}'
+            stocks_total=f'{df_total_count["stocks_count"].sum():,}'
+
+            cols=st.columns(4)
+            with cols[0]:
+                show_articles(articles)
+            with cols[1]:
+                show_view_total(view_total)
+            with cols[2]:
+                show_likes_total(likes_total)
+            with cols[3]:
+                show_stocks_total(stocks_total)
+
+            show_wordcloud(wordcloud_text)
+
+            st.write("## Details")
+            if len(df_likes):
+                df_likes["created_at"]=df_likes["created_at"].str[0:13]+":00:00"
+                df_likes["created_at"]=pd.to_datetime(df_likes["created_at"])
+                df_likes["created_at"]=df_likes["created_at"].dt.strftime('%Y/%m/%d')
+
+
+            for id, sdf in df_total_count.sort_values(
+                sort_options[sort_value_jp], ascending=False
+            ).groupby("id", sort=False):
+                likes=pd.DataFrame()
+
+                if len(df_likes):
+                    likes = df_likes[df_likes["id"]==id][["created_at","id"]].groupby("created_at").count()
+                    likes["likes"] = likes["id"].cumsum()
+                    likes=likes.drop("id",axis=1)
+
+                title=sdf["title"].values[0]
+                title_caption = sdf[sort_options[sort_value_jp]].values[0]
+                with st.expander(f"【{sort_value_jp} / {title_caption}】\t{title} "):
+                    st.write(f'<a href="{sdf["url"].values[0]}">{title}</a>',unsafe_allow_html=True)
+                    cols=st.columns(4)
+                    with cols[0]:
+                        show_view_total(sdf["page_views_count"].values[0])
+                    with cols[1]:
+                        show_likes_total(sdf["likes_count"].values[0])
+                    with cols[2]:
+                        show_stocks_total(sdf["stocks_count"].values[0])
+                    st.line_chart(likes)
+
+if __name__ == "__main__":
+    main()
